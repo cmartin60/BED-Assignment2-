e937c704c3a7e873efdd52ff81f1061f5456929c
Christian Martin <cmartin60@academic.rrc.ca>
2025-03-18 23:13:50 -0500

    Add all the basic files needed for assignment 2

---
+++ src/app.ts
@@ -0,0 +1,45 @@
+// import the express application and type definition
+import express, { Express } from "express";
+import morgan from "morgan";
+
+// import setupSwagger endpoint
+import setupSwagger from "../config/swagger";
+
+// initialize the express application
+const app: Express = express();
+
+// setup swagger for api documentation
+setupSwagger(app);
+
+app.use(morgan("combined"));
+app.use(express.json());
+
+// respond to GET request at endpoint "/" with message
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+// example "tasks" endpoint
+/**
+ * @openapi
+ * /tasks:
+ *  get:
+ *   summary: Retrieve a list of tasks
+ *   tags: [Tasks]
+ *   responses:
+ *    200:
+ *     description: A list of tasks
+ */
+app.get("/tasks", (req, res) => {
+  res.send("Retrieve tasks");
+});
+
+
+// Health Check Endpoint
+app.get("/health", (req, res) => {
+	res.status(200).send("Server is healthy");
+});
+
+
+// export app and server for testing
+export default app;
